1a36715f300178a6e240cad7e1bb7f9a195313b8
Vuli Russell <vulirussell@gmail.com>
2020-11-19 12:13:51 +0000

    quiz working

--- src/components/QuizApp/QuizApp.jsx
+++ src/components/QuizApp/QuizApp.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./QuizApp.module.scss";
+import { useForm } from "react-hook-form";
 
 const QuizApp = () => {
+  const { register, handleSubmit} = useForm();
+
+  const onSubmit = data => {
+    setResponse(data.answer.match(/kanye/i)? true : data.answer);
+  }
+
+  const [response, setResponse] = useState(null)
+
   return (
     <>
-      <p>QuizApp works</p>
+      <main className={styles.quiz}>
+        <h1>Genius Quiz 2020</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="answer">Who is the foremost World Genius Right Now?</label>
+          <br/>
+          <input type="text" name="answer" id="answer" ref={register({required: true})}/>
+          <br/>
+          <input type="submit"/>
+        </form>
+        {
+          response===true ? 
+          <div className={`${styles.answer} ${styles.right}`}>
+            🌊Correct, Kayne is pretty much god🌊
+          </div>
+          :
+          response ?
+          <div className={`${styles.answer} ${styles.wrong}`}>
+            {`${response} is not the foremost genius right now, Please try again`}
+          </div>
+          :
+          null
+        }
+      </main>
     </>
   );
 };
