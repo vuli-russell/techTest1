dd402e8e7ac854c77ab43909be5d7207b9cc5730
Vuli Russell <vulirussell@gmail.com>
2020-11-19 10:53:44 +0000

    some header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import { faFutbol, faSun, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from "@reach/router";
 
 const Header = () => {
   return (
-    <>
+    <header className={styles.header}>
       <h1>Vuli Russell Tech Test</h1>
-    </>
+      <div className={styles.links}>
+        <div className={styles.link}>
+          <Link to="/"><FontAwesomeIcon icon={faSun} /></Link>
+        </div>
+        <div className={styles.link}>
+          <Link to="/quiz"><FontAwesomeIcon icon={faQuestionCircle} /></Link>
+        </div>
+        <div className={styles.link}>
+          <Link to="/football"><FontAwesomeIcon icon={faFutbol} /></Link>
+        </div>
+      </div>
+    </header>
   );
 };
 
